ce54a406ab2c7888263158383c748c4bc0b7a0bb
Fumitaka Hijino <Hijino@hijino-no-macbook-puro-2017.local>
2018-01-28 19:35:53 +0900

    realmデータベースの準備完了

---
+++ taskapp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by Fumitaka Hijino on 2018/01/28.
+//  Copyright © 2018年 Fumitaka Hijino. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    /// 日時
+    @objc dynamic var date = Date()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
